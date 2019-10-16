8a6a5edff4176af4ab7d5e40b5ee85fd34bb00b0
edwardsrob222 <edwardsrob222@gmail.com>
2019-10-16 14:25:11 -0400

    updates accounts urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,5 +8,5 @@ app_name = 'accounts'
 urlpatterns = [
     path('<int:pk>/profile/', views.ProfileDetailView.as_view(), name="profile_detail"),
     path('profile/', views.ProfileCreateView.as_view(), name='profile'),
-    path('signup/', views.SignUp.as_view(), name='signup'),
+    path('signup/', views.SignUpView.as_view(), name='signup'),
 ]
